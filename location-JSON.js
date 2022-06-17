1ffa2e0f62d36c72a7e4938b92ca8da584bd35dc
Sasha Lukas <51677632+slukas111@users.noreply.github.com>
2022-06-16 23:45:54 -0700

    created json for locations- chad

---
+++ location-JSON.js
@@ -0,0 +1,38 @@
+/*!
+	Use of JSON
+*/
+var loca = {
+
+	"state" : [
+		"Idaho",
+		"Oregon",
+		"Washington"
+	],
+
+	"Idaho" : [
+		{ "addr" : "1234 Main St <br/>", "city" : "Boise, ", "stat": "ID ", "zip" : "12345 <br/> ", "phone" : "Phone 011-555-1234 <br/><br/>" },
+		{ "addr" : "1st St <br/>", "city" : "Lewiston, ", "stat": "ID ", "zip" : "12357 <br/>  ", "phone" : "Phone 101-555-1235 <br/><br/>" },
+		{ "addr" : "34 Pine St <br/>", "city" : "Sandpoint, ", "stat": "ID ", "zip" : "12354 <br/>  ", "phone" : "Phone 111-555-1236 <br/> <br/>" },
+		{ "addr" : "23 River St <br/>", "city" : "Nampa, ", "stat": "ID ", "zip" : "12333 <br/>  ", "phone" : "Phone 111-555-1273  <br/><br/>" }
+		
+	],
+	
+	"Oregon" : [ 
+		{ "addr" : "360 Blazer St <br/>", "city" : "Portland, ", "stat": "OR ", "zip" : "45678 <br/>", "phone" : "Phone 111-555-1234  <br/><br/>" },
+		{ "addr" : "782 Elm St <br/>", "city" : "Salem, ", "stat": "OR ", "zip" : "42345 <br/>", "phone" : "Phone 111-555-1234  <br/><br/>" },
+		{ "addr" : "769 10th Ave <br/>", "city" : "Corvallis, ", "stat": "OR ", "zip" : "42366 <br/>", "phone" : "Phone 111-555-1234  <br/><br/>" },
+		{ "addr" : "234 Falls St<br/>", "city" : "Klamath Falls, ", "stat": "OR ", "zip" : "41345 <br/>", "phone" : "Phone 111-555-1234 <br/><br/>" }
+		
+	],
+	
+	"Washington" : [ 
+		{ "addr" : "989 Park St <br/>", "city" : "Poulsbo, ", "stat": "WA ", "zip" : "99123 <br/>", "phone" : "Phone 112-555-1234  <br/><br/>" },
+		{ "addr" : "650 Ferry St <br/>", "city" : "Bremerton, ", "stat": "WA ", "zip" : "99134 <br/>", "phone" : "Phone 113-555-1234  <br/><br/>" },
+		{ "addr" : "778 Gum Wall St <br/>", "city" : "Seattle, ", "stat": "WA ", "zip" : "99345 <br/>", "phone" : "Phone 114-555-1234  <br/><br/>" },
+		{ "addr" : "3332 Capitol St <br/>", "city" : "Olympia, ", "stat": "WA ", "zip" : "99500 <br/>", "phone" : "Phone 115-555-1234  <br/><br/>" },
+		{ "addr" : "909 Columbia St <br/>", "city" : "Vancouver, ", "stat": "WA ", "zip" : "99366 <br/>", "phone" : "Phone 116-555-1234  <br/><br/>" }
+		
+	],
+
+
+};
